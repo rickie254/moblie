1f6cc039944a4ae0aebf3932d225d5589f375e35
rickie maweu <rickie.maweu@student.moringaschool.com>
2023-03-19 09:28:35 +0300

    page one

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Image, View, TextInput } from "react-native";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/Header.jpg")} style={styles.image} />
+      <TextInput
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        style={styles.input}
+        secureTextEntry={true}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  input: {
+    width:300,
+    margin:12,
+    padding:3,
+    borderbottomwidth:1,
   },
 });
